8e999329acc2dc6a4d94cd8629efbcfa20ef8b87
jlmaring <johny.marin@udea.edu.co>
2023-10-10 23:00:57 -0500

    sagregan mas test

--- src/test/java/com/example/democrud/PersonaTest.java
+++ src/test/java/com/example/democrud/PersonaTest.java
@@ -1,3 +1,6 @@
+package com.example.democrud;
+
+import com.example.democrud.model.Persona;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
@@ -6,7 +9,28 @@ public class PersonaTest {
     @Test
     public void testGetNombre() {
         Persona persona = new Persona();
-        persona.setNombre("Juan");
-        assertEquals("Juan", persona.getNombre());
+        persona.setNombre("Johny");
+        assertEquals("Johny", persona.getNombre());
     }
+
+    @Test
+    public void testSetNombre() {
+        Persona persona = new Persona();
+        persona.setApellido("Marin");
+    }
+
+    @Test
+    public void testSetDireccion(){
+        Persona persona = new Persona();
+        persona.setDireccion("Calle falsa 123");
+        assertEquals("Calle falsa 123", persona.getDireccion());
+    }
+
+    @Test
+    public void testSetTelefono(){
+        Persona persona = new Persona();
+        persona.setTelefono("604-1234");
+        assertEquals("604-1234", persona.getTelefono());
+    }
+
 }
--- src/test/java/com/example/democrud/UsuarioTest.java
+++ src/test/java/com/example/democrud/UsuarioTest.java
@@ -1,26 +1,37 @@
 package com.example.democrud;
 
+import com.example.democrud.model.Usuario;
 import org.junit.Test;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import static org.junit.Assert.*;
 
 public class UsuarioTest {
 	
-
 	@Test
 	public void testGetUsuario(){
 		Usuario usuario = new Usuario();
-		usuario.setUsuario("Juan");
-		assertEquals("Juan", usuario.getUsuario());
+		usuario.setUsuario("Johny");
+		assertEquals("Johny", usuario.getUsuario());
 	}
-	
+
+	@Test
+	public void testSetClave(){
+		Usuario usuario = new Usuario();
+		usuario.setClave("password");
+		assertEquals("password", usuario.getClave());
+	}
+
+	@Test
+	public void testSetRoleName(){
+		Usuario usuario = new Usuario();
+		usuario.setRoleName("administrador");
+		assertEquals("administrador", usuario.getRoleName());
+	}
 
 	@Test
 	public void generatePassword() {
 		BCryptPasswordEncoder passGen = new BCryptPasswordEncoder();
 		System.out.println(passGen.encode("admin"));
 	}
-
-
 
 }
